eb059c0250bb949dca1fa10b15cad21e101eedd0
Juan Esteban <juanestebanrp123@gmail.com>
2025-09-05 11:24:47 -0500

    detalle

--- src/screens/LandingPageProveedores/LandingPageProveedores.jsx
+++ src/screens/LandingPageProveedores/LandingPageProveedores.jsx
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import {CompanyDescriptionProveedores,FeaturesCardsProveedores,HeroSectionProveedor,FAQCardsProveedores,TestimonialsSection,CallToAction} from '../../components/index'
 
 export function LandingPageProveedores() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="bg-white">
       <HeroSectionProveedor />
